1943c0693c7e207b1a8edd1518c85a8e548494c0
ColtonWright51 <cw717317@ohio.edu>
2022-12-21 15:52:32 -0500

    Unreliable connection

---
+++ src/modules/modem_helper.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+
+
+def hello_world():
+    print("hello_world")
+
+
+def make_data_file():
+    now = datetime.datetime.today()
+    now = now.strftime("%Y-%m-%d")
+    data_path = os.path.join("data", now+"_data")
+    os.makedirs(data_path, exist_ok=True)
+    now = datetime.datetime.today()
+    now = now.strftime("%Y-%m-%dT%H%M%S")
+    log_file_path = os.path.join(data_path, now+"_log")
+    return(log_file_path)
--- src/prog1.py
+++ src/prog1.py
@@ -1,31 +1,28 @@
-"""Provides NumberList and FrequencyDistribution, classes for statistics.
-
-NumberList holds a sequence of numbers, and defines several statistical
-operations (mean, stdev, etc.) FrequencyDistribution holds a mapping from
-items (not necessarily numbers) to counts, and defines operations such as
-Shannon entropy and frequency normalization.
 """
+Communicates to the Quectel RM502Q-AE modem through a serial port. The modem
+uses the standard "Hayes command set" popular with modems.
 
-"""
-
+https://en.wikipedia.org/wiki/Hayes_command_set
 
 """
 
 import serial
 import os
 import datetime
+import modules.modem_helper as modem_helper
 
-now = datetime.datetime.today()
-now = now.strftime("%Y-%m-%d")
-data_path = os.path.join("data", now+"_data")
-os.makedirs(data_path, exist_ok=True)
-now = datetime.datetime.today()
-now = now.strftime("%Y-%m-%dT%H%M%S")
-log_file_path = os.path.join(data_path, now+"_log")
+# Setup data directory & file name to save responses from modem
+log_file_path = modem_helper.make_data_file()
+
+time_to_read = .1
+time_to_end = datetime.datetime.today().timestamp() + 60*time_to_read
 
 with serial.Serial('/dev/ttyUSB3', baudrate=115200, timeout=1) as ser, \
-    open(log_file_path, 'w') as file:
+    open(log_file_path, 'ab') as file:
     
-    ser.write(b"AT")
-    read1 = ser.read(30)
-    file.write(read1.decode())
+    ser.write(b"at")
+    file.write(ser.read(100))
+
+    
+    # while datetime.datetime.today().timestamp() < time_to_end:
+        # file.write(ser.read(100))
