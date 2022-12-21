424e8094822eba62c433bf477153ab56f0508b7d
ColtonWright51 <cw717317@ohio.edu>
2022-12-21 17:03:07 -0500

    More messages, t.sleep to allow for processing of command

--- src/prog1.py
+++ src/prog1.py
@@ -10,6 +10,7 @@ import serial
 import os
 import datetime
 import modules.modem_helper as modem_helper
+import time
 
 # Setup data directory & file name to save responses from modem
 log_file_path = modem_helper.make_data_file()
@@ -21,19 +22,54 @@ with serial.Serial('/dev/ttyUSB3', baudrate=115200, timeout=1) as ser, \
     open(log_file_path, 'ab') as file:
     
     # All commands must start with AT or at and end with carriage return!
+    ser.write(b"AT&F0")
+    file.write(ser.read(100))
+    time.sleep(.3)
     ser.write(b"ATE1\r")
     file.write(ser.read(100))
+    time.sleep(.3)
+
     ser.write(b"ATI\r")
     file.write(ser.read(100))
+    time.sleep(.3)
     ser.write(b"AT+GMI\r")
     file.write(ser.read(100))
+    time.sleep(.3)
+
     ser.write(b"AT+GMM\r")
     file.write(ser.read(100))
+    time.sleep(.3)
+
     ser.write(b"AT+GSN\r")
     file.write(ser.read(100))
+    time.sleep(.3)
+
     ser.write(b"AT+CGSN\r")
     file.write(ser.read(100))
+    time.sleep(.3)
+
     ser.write(b"AT&V\r")
-    file.write(ser.read(150))
-    # while datetime.datetime.today().timestamp() < time_to_end:
-        # file.write(ser.read(100))
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    time.sleep(.3)
+
+    ser.write(b"AT+QCFG=?\r")
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    file.write(ser.read(100))
+    time.sleep(.3)
+
+    ser.write(b"AT+CIMI=?\r")
+    file.write(ser.read(100))
+    time.sleep(.3)
+
+    ser.write(b"AT+CIMI\r")
+    file.write(ser.read(100))
+    
+    time.sleep(.3)
+    ser.close()
+    file.close()
