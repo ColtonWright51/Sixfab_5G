70ab5cf94fbe3e10774c19a96792ce199f52c9a6
ColtonWright51 <cw717317@ohio.edu>
2022-12-22 14:37:36 -0500

    Document code

--- src/prog1.py
+++ src/prog1.py
@@ -22,37 +22,45 @@ with serial.Serial('/dev/ttyUSB3', baudrate=115200, timeout=1) as ser, \
     open(log_file_path, 'ab') as file:
     
     # All commands must start with AT or at and end with carriage return!
+
+    # Reset AT command settings to factory settings
     ser.write(b"AT&F0")
     file.write(ser.read(100))
     time.sleep(.3)
+
+    # Set command echo mode
     ser.write(b"ATE1\r")
     file.write(ser.read(100))
     time.sleep(.3)
 
+
+    # Display MT identification information
     ser.write(b"ATI\r")
     file.write(ser.read(100))
     time.sleep(.3)
+
+    # Request manufacturer identification
     ser.write(b"AT+GMI\r")
     file.write(ser.read(100))
     time.sleep(.3)
 
+    # Request model identification
     ser.write(b"AT+GMM\r")
     file.write(ser.read(100))
     time.sleep(.3)
 
+    # Request international mobile equipment identity
     ser.write(b"AT+GSN\r")
     file.write(ser.read(100))
     time.sleep(.3)
 
-    ser.write(b"AT+CGSN\r")
-    file.write(ser.read(100))
-    time.sleep(.3)
-
+    # Display current configuration
     ser.write(b"AT&V\r")
     file.write(ser.read(100))
     file.write(ser.read(100))
     time.sleep(.3)
 
+    # Test command extended config settings
     ser.write(b"AT+QCFG=?\r")
     file.write(ser.read(100))
     file.write(ser.read(100))
@@ -63,13 +71,13 @@ with serial.Serial('/dev/ttyUSB3', baudrate=115200, timeout=1) as ser, \
     file.write(ser.read(100))
     time.sleep(.3)
 
+    # Test command Request International Mobile Subscriber Identity (IMSI)
     ser.write(b"AT+CIMI=?\r")
     file.write(ser.read(100))
     time.sleep(.3)
 
+    # Request International Mobile Subscriber Identity (IMSI)
     ser.write(b"AT+CIMI\r")
     file.write(ser.read(100))
-    
     time.sleep(.3)
-    ser.close()
-    file.close()
+
