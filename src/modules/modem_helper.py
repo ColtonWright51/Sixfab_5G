e57196b29e8f52c6c983f43e5c8dff42b6bc4df2
ColtonWright51 <cw717317@ohio.edu>
2023-01-04 14:51:37 -0500

    More tests

--- src/modules/modem_helper.py
+++ src/modules/modem_helper.py
@@ -13,7 +13,7 @@ def make_data_file():
     log_file_path = os.path.join(data_path, now+"_log")
     return(log_file_path)
 
-def automate_test_read(ser, file, command, t_sleep=0.3):
+def automate_test_read(ser, file, command, read=True, t_sleep=0.3):
     # Send faster command
     bin_command = command.encode("ascii")
     ser.write(b"AT+"+bin_command+b"=?\r")
@@ -24,8 +24,10 @@ def automate_test_read(ser, file, command, t_sleep=0.3):
 
     # This will give errors if command does not exist. Just will have
     # another error in the text file, it's fine.
-    ser.write(b"AT+"+bin_command+b"?\r")
-    time.sleep(t_sleep)
-    file.write(ser.read(100))
-    file.write(ser.read(100))
-    file.write(ser.read(100))
+
+    if(read):
+        ser.write(b"AT+"+bin_command+b"?\r")
+        time.sleep(t_sleep)
+        file.write(ser.read(100))
+        file.write(ser.read(100))
+        file.write(ser.read(100))
